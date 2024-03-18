1a2cd04eb0dce580526660bbc27173a08e5897b7
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-03-19 01:40:51 +0600

    add.c

--- 1-Introduction-to-C/add.c
+++ 1-Introduction-to-C/add.c
@@ -3,9 +3,10 @@
 int main(int argc, char const *argv[])
 {
     int a, b;
-    printf("Enter number a");
+    printf("Enter number a\t");
     scanf("%d", &a);
+    printf("Enter Number b\t");
     scanf("%d", &b);
-    printf("Enter Number b");
+    printf("The sum is %d\t", a+b);
     return 0;
 }
