b82d602026d975111c6f30b3000e3c8f89d295a6
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-03-30 23:24:51 +0600

    variables and data types in c programming

---
+++ C-Practice/Module 1/DV.c
@@ -0,0 +1,15 @@
+// Data Types and Variables
+#include <stdio.h>
+
+int main (){
+    return 0;
+}
+
+
+/*
+Example of Data Types :
+int -> -100, 0, 100
+float -> -2.5, 5.45
+char -> "a", 'A', '@'  <- [It must be]
+bool -> true or false
+*/
