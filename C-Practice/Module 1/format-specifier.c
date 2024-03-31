30141cd76e97ca2d9db13a13d8a7f13d0d9a2372
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-03-31 14:12:07 +0600

    directory changed

---
+++ C-Practice/Module 1/format-specifier.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    float pi = 3.14159;
+    char letter = 'A';
+    char str[] = "Hello";
+
+    printf("Integer: %d\n", num);
+    printf("Float: %f\n", pi);
+    printf("Character: %c\n", letter);
+    printf("String: %s\n", str);
+
+    return 0;
+}
