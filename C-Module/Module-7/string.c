b76e5d6fa4b2da771fcb7a6962ebd0b3054fc606
Hasnat Shahriyar <121642545+hasnat-shahriyar@users.noreply.github.com>
2024-04-22 16:36:01 +0600

    Delete C-Module directory

--- C-Module/Module-7/string.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    char *str1 = "Hello, apni kemon achen?";
-    char str[] = "Bye! abar dekha hobe";
-    printf("String 1: %s\n", str1);
-    printf("String 2: %s\n", str);
-    return 0;
-}
