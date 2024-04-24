67cdd41019737d55405f257e0b6a048101738f56
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-04-24 21:57:50 +0600

    frequency array

--- C-Practice/Module-8/frequency_array1.c
+++ C-Practice/Module-8/frequency_array1.c
@@ -8,13 +8,13 @@ int main(){
     {
         scanf("%d", &a[i]);
     }
-    int cnt[6] = {0}; // Changed size to 6 and initialized all elements to 0
+    int cnt[6] = {0};
     for (int i = 0; i < n; i++)
     {
         int val = a[i];
         cnt[val]++;        
     }
-    for (int i = 0; i < 6; i++) // Changed loop condition to iterate up to 5
+    for (int i = 0; i < 6; i++)
     {
         printf("%d - %d\n", i, cnt[i]);
     }
