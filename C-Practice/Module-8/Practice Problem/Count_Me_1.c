0b0122df2bbfcea263bfb5962eccf66e69b3c5ea
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-04-27 18:26:12 +0600

    practice problem solving

---
+++ C-Practice/Module-8/Practice Problem/Count_Me_1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    int two = 0;
+    int three = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0 && a[i] % 3 != 0)
+        {
+            two++;
+        }
+        if (a[i] % 3 == 0 && a[i] % 2 != 0)
+        {
+            three++;
+        }
+        if (a[i] % 2 == 0 && a[i] % 3 == 0)
+        {
+            two++;
+        }
+    }
+    printf("%d %d", two, three);
+      
+    return 0;
+}
